1b1aa03699d80936b46679c44fdb0105a8464f07
ImBelo <gianmarco.beligni@gmail.com>
2026-06-01 14:05:20 +0200

    camera was only blocked in wsl, fixed allocation redundancy and made movement vector be horizontal

--- Voxel-Panama/main.scala
+++
@@ -1,6 +0,0 @@
-import Window.*
-object PanamaGlfwWindow {
-  def main(args: Array[String]): Unit = {
-    Window.start(1200,800,"Dio cane")
-  }
-}
--- Voxel-Panama/project.scala
+++
@@ -1,4 +0,0 @@
-//> using javaOpt --enable-preview
-//> using javaOpt --enable-native-access=ALL-UNNAMED
-//> using javaProp java.library.path=/usr/lib:/usr/local/lib:/opt/homebrew/lib
-//> using dep org.joml:joml:1.10.8
